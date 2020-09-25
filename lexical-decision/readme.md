0c515f7882b58f3ad6ce1ded52eaff5253a0b9f7
Josh de Leeuw <josh.deleeuw@gmail.com>
2020-09-25 11:33:30 -0400

    add lexical decision (CSV) tutorial

---
+++ lexical-decision/readme.md
@@ -0,0 +1,11 @@
+# Lexical Decision Experiment
+
+This tutorial is mostly aimed at understanding how to get data from a .csv file into your jsPsych experiment. For example, if you have trial information in a spreadsheet and you want to use that to create your jsPsych trials.
+
+## Video Link
+
+[Watch the tutorial on YouTube](https://www.youtube.com/watch?v=T3GCZWarEsY)
+
+## Experiment link
+
+[Try the completed experiment](https://jspsych.github.io/tutorials/lexical-decision/)
---
+++ lexical-decision/stimuli.js
@@ -0,0 +1,752 @@
+var test_stimuli = [
+  {
+    "prime": "PHRASE",
+    "target": "sentence",
+    "condition": "semantic"
+  },
+  {
+    "prime": "SEA",
+    "target": "ocean",
+    "condition": "semantic"
+  },
+  {
+    "prime": "PRIMATE",
+    "target": "monkey",
+    "condition": "semantic"
+  },
+  {
+    "prime": "SCARE",
+    "target": "fright",
+    "condition": "semantic"
+  },
+  {
+    "prime": "SYNOPSIS",
+    "target": "summary",
+    "condition": "semantic"
+  },
+  {
+    "prime": "WEAKNESS",
+    "target": "strength",
+    "condition": "semantic"
+  },
+  {
+    "prime": "RUG",
+    "target": "carpet",
+    "condition": "semantic"
+  },
+  {
+    "prime": "CRAB",
+    "target": "lobster",
+    "condition": "semantic"
+  },
+  {
+    "prime": "CHAIR",
+    "target": "table",
+    "condition": "semantic"
+  },
+  {
+    "prime": "FURY",
+    "target": "anger",
+    "condition": "semantic"
+  },
+  {
+    "prime": "COWARD",
+    "target": "wimp",
+    "condition": "semantic"
+  },
+  {
+    "prime": "COD",
+    "target": "fish",
+    "condition": "semantic"
+  },
+  {
+    "prime": "SABER",
+    "target": "sword",
+    "condition": "semantic"
+  },
+  {
+    "prime": "YEARS",
+    "target": "age",
+    "condition": "semantic"
+  },
+  {
+    "prime": "CHARACTER",
+    "target": "personality",
+    "condition": "semantic"
+  },
+  {
+    "prime": "SILK",
+    "target": "satin",
+    "condition": "semantic"
+  },
+  {
+    "prime": "HYPOTHESIS",
+    "target": "guess",
+    "condition": "semantic"
+  },
+  {
+    "prime": "POET",
+    "target": "writer",
+    "condition": "semantic"
+  },
+  {
+    "prime": "CREVICE",
+    "target": "crack",
+    "condition": "semantic"
+  },
+  {
+    "prime": "WICK",
+    "target": "candle",
+    "condition": "semantic"
+  },
+  {
+    "prime": "SLACKS",
+    "target": "pants",
+    "condition": "semantic"
+  },
+  {
+    "prime": "POND",
+    "target": "lake",
+    "condition": "semantic"
+  },
+  {
+    "prime": "REGION",
+    "target": "area",
+    "condition": "semantic"
+  },
+  {
+    "prime": "HANDS",
+    "target": "feet",
+    "condition": "semantic"
+  },
+  {
+    "prime": "PEBBLE",
+    "target": "rock",
+    "condition": "semantic"
+  },
+  {
+    "prime": "KEY",
+    "target": "lock",
+    "condition": "associative"
+  },
+  {
+    "prime": "HOSTESS",
+    "target": "restaurant",
+    "condition": "associative"
+  },
+  {
+    "prime": "CRUTCH",
+    "target": "leg",
+    "condition": "associative"
+  },
+  {
+    "prime": "LITTER",
+    "target": "cats",
+    "condition": "associative"
+  },
+  {
+    "prime": "SEASHORE",
+    "target": "shell",
+    "condition": "associative"
+  },
+  {
+    "prime": "ADMISSION",
+    "target": "ticket",
+    "condition": "associative"
+  },
+  {
+    "prime": "PITCHFORK",
+    "target": "hay",
+    "condition": "associative"
+  },
+  {
+    "prime": "MIRAGE",
+    "target": "desert",
+    "condition": "associative"
+  },
+  {
+    "prime": "SALAD",
+    "target": "lettuce",
+    "condition": "associative"
+  },
+  {
+    "prime": "DIPLOMA",
+    "target": "graduation",
+    "condition": "associative"
+  },
+  {
+    "prime": "LIGHTNING",
+    "target": "thunder",
+    "condition": "associative"
+  },
+  {
+    "prime": "CAMERA",
+    "target": "picture",
+    "condition": "associative"
+  },
+  {
+    "prime": "ART",
+    "target": "paint",
+    "condition": "associative"
+  },
+  {
+    "prime": "THRONE",
+    "target": "king",
+    "condition": "associative"
+  },
+  {
+    "prime": "MOP",
+    "target": "floor",
+    "condition": "associative"
+  },
+  {
+    "prime": "ELECTRICIAN",
+    "target": "wire",
+    "condition": "associative"
+  },
+  {
+    "prime": "BALL",
+    "target": "bat",
+    "condition": "associative"
+  },
+  {
+    "prime": "CLOUD",
+    "target": "sky",
+    "condition": "associative"
+  },
+  {
+    "prime": "HAT",
+    "target": "head",
+    "condition": "associative"
+  },
+  {
+    "prime": "RENT",
+    "target": "apartment",
+    "condition": "associative"
+  },
+  {
+    "prime": "TUXEDO",
+    "target": "prom",
+    "condition": "associative"
+  },
+  {
+    "prime": "ARROW",
+    "target": "bow",
+    "condition": "associative"
+  },
+  {
+    "prime": "LAVA",
+    "target": "volcano",
+    "condition": "associative"
+  },
+  {
+    "prime": "ASPIRIN",
+    "target": "headache",
+    "condition": "associative"
+  },
+  {
+    "prime": "ALLERGY",
+    "target": "sneeze",
+    "condition": "associative"
+  },
+  {
+    "prime": "FUEL",
+    "target": "ability",
+    "condition": "unrelated"
+  },
+  {
+    "prime": "LION",
+    "target": "religion",
+    "condition": "unrelated"
+  },
+  {
+    "prime": "PROMISE",
+    "target": "magic",
+    "condition": "unrelated"
+  },
+  {
+    "prime": "FAME",
+    "target": "material",
+    "condition": "unrelated"
+  },
+  {
+    "prime": "SIDE",
+    "target": "bite",
+    "condition": "unrelated"
+  },
+  {
+    "prime": "IGLOO",
+    "target": "mean",
+    "condition": "unrelated"
+  },
+  {
+    "prime": "FOSSIL",
+    "target": "machine",
+    "condition": "unrelated"
+  },
+  {
+    "prime": "LOSS",
+    "target": "boots",
+    "condition": "unrelated"
+  },
+  {
+    "prime": "CARTON",
+    "target": "spinach",
+    "condition": "unrelated"
+  },
+  {
+    "prime": "GUYS",
+    "target": "questions",
+    "condition": "unrelated"
+  },
+  {
+    "prime": "DEVIL",
+    "target": "club",
+    "condition": "unrelated"
+  },
+  {
+    "prime": "CONDITIONER",
+    "target": "football",
+    "condition": "unrelated"
+  },
+  {
+    "prime": "JEWEL",
+    "target": "service",
+    "condition": "unrelated"
+  },
+  {
+    "prime": "WITCH",
+    "target": "apology",
+    "condition": "unrelated"
+  },
+  {
+    "prime": "PASSAGE",
+    "target": "poison",
+    "condition": "unrelated"
+  },
+  {
+    "prime": "TOSS",
+    "target": "builder",
+    "condition": "unrelated"
+  },
+  {
+    "prime": "SCARF",
+    "target": "turn",
+    "condition": "unrelated"
+  },
+  {
+    "prime": "FAN",
+    "target": "skin",
+    "condition": "unrelated"
+  },
+  {
+    "prime": "DIP",
+    "target": "action",
+    "condition": "unrelated"
+  },
+  {
+    "prime": "BORDER",
+    "target": "dollars",
+    "condition": "unrelated"
+  },
+  {
+    "prime": "COAST",
+    "target": "actor",
+    "condition": "unrelated"
+  },
+  {
+    "prime": "CORPORAL",
+    "target": "princess",
+    "condition": "unrelated"
+  },
+  {
+    "prime": "IDOL",
+    "target": "rose",
+    "condition": "unrelated"
+  },
+  {
+    "prime": "CONVENT",
+    "target": "beginner",
+    "condition": "unrelated"
+  },
+  {
+    "prime": "CHAOS",
+    "target": "bag",
+    "condition": "unrelated"
+  },
+  {
+    "prime": "HIKE",
+    "target": "argel",
+    "condition": "nonword"
+  },
+  {
+    "prime": "WASHCLOTH",
+    "target": "alfeity",
+    "condition": "nonword"
+  },
+  {
+    "prime": "CAUTION",
+    "target": "arzy",
+    "condition": "nonword"
+  },
+  {
+    "prime": "TREE",
+    "target": "agom",
+    "condition": "nonword"
+  },
+  {
+    "prime": "LINGERIE",
+    "target": "altic",
+    "condition": "nonword"
+  },
+  {
+    "prime": "SYSTEM",
+    "target": "beel",
+    "condition": "nonword"
+  },
+  {
+    "prime": "TWILIGHT",
+    "target": "boint",
+    "condition": "nonword"
+  },
+  {
+    "prime": "THIEF",
+    "target": "bix",
+    "condition": "nonword"
+  },
+  {
+    "prime": "DEVELOPMENT",
+    "target": "calinet",
+    "condition": "nonword"
+  },
+  {
+    "prime": "PINT",
+    "target": "zard",
+    "condition": "nonword"
+  },
+  {
+    "prime": "MICROWAVE",
+    "target": "cags",
+    "condition": "nonword"
+  },
+  {
+    "prime": "DELIGHT",
+    "target": "fice",
+    "condition": "nonword"
+  },
+  {
+    "prime": "SLEIGH",
+    "target": "troice",
+    "condition": "nonword"
+  },
+  {
+    "prime": "MYTH",
+    "target": "cleck",
+    "condition": "nonword"
+  },
+  {
+    "prime": "MOONLIGHT",
+    "target": "frops",
+    "condition": "nonword"
+  },
+  {
+    "prime": "ASH",
+    "target": "curlains",
+    "condition": "nonword"
+  },
+  {
+    "prime": "PIECES",
+    "target": "earsh",
+    "condition": "nonword"
+  },
+  {
+    "prime": "PATIO",
+    "target": "epenator",
+    "condition": "nonword"
+  },
+  {
+    "prime": "FOAM",
+    "target": "elerfy",
+    "condition": "nonword"
+  },
+  {
+    "prime": "KILLER",
+    "target": "foad",
+    "condition": "nonword"
+  },
+  {
+    "prime": "INSTINCT",
+    "target": "fost",
+    "condition": "nonword"
+  },
+  {
+    "prime": "RAYS",
+    "target": "bortuse",
+    "condition": "nonword"
+  },
+  {
+    "prime": "LIBEL",
+    "target": "glores",
+    "condition": "nonword"
+  },
+  {
+    "prime": "TENDENCY",
+    "target": "prill",
+    "condition": "nonword"
+  },
+  {
+    "prime": "SURGERY",
+    "target": "vall",
+    "condition": "nonword"
+  },
+  {
+    "prime": "TRAUMA",
+    "target": "pome",
+    "condition": "nonword"
+  },
+  {
+    "prime": "CUP",
+    "target": "dorn",
+    "condition": "nonword"
+  },
+  {
+    "prime": "SISTER",
+    "target": "gula",
+    "condition": "nonword"
+  },
+  {
+    "prime": "SOUND",
+    "target": "duman",
+    "condition": "nonword"
+  },
+  {
+    "prime": "MOVIE",
+    "target": "hulger",
+    "condition": "nonword"
+  },
+  {
+    "prime": "ATTENTION",
+    "target": "wob",
+    "condition": "nonword"
+  },
+  {
+    "prime": "PUNCTUATION",
+    "target": "litcher",
+    "condition": "nonword"
+  },
+  {
+    "prime": "COPPER",
+    "target": "lind",
+    "condition": "nonword"
+  },
+  {
+    "prime": "TOURIST",
+    "target": "pemon",
+    "condition": "nonword"
+  },
+  {
+    "prime": "DISHES",
+    "target": "dength",
+    "condition": "nonword"
+  },
+  {
+    "prime": "PHASE",
+    "target": "lespers",
+    "condition": "nonword"
+  },
+  {
+    "prime": "THRESHOLD",
+    "target": "moil",
+    "condition": "nonword"
+  },
+  {
+    "prime": "CHIP",
+    "target": "reat",
+    "condition": "nonword"
+  },
+  {
+    "prime": "TANGERINE",
+    "target": "roney",
+    "condition": "nonword"
+  },
+  {
+    "prime": "REPUBLIC",
+    "target": "pinease",
+    "condition": "nonword"
+  },
+  {
+    "prime": "MASK",
+    "target": "nenk",
+    "condition": "nonword"
+  },
+  {
+    "prime": "DOORBELL",
+    "target": "vight",
+    "condition": "nonword"
+  },
+  {
+    "prime": "ROD",
+    "target": "nocine",
+    "condition": "nonword"
+  },
+  {
+    "prime": "FITNESS",
+    "target": "samper",
+    "condition": "nonword"
+  },
+  {
+    "prime": "BOOK",
+    "target": "onster",
+    "condition": "nonword"
+  },
+  {
+    "prime": "MASSAGE",
+    "target": "pranet",
+    "condition": "nonword"
+  },
+  {
+    "prime": "DOLL",
+    "target": "povice",
+    "condition": "nonword"
+  },
+  {
+    "prime": "CUSTARD",
+    "target": "rabe",
+    "condition": "nonword"
+  },
+  {
+    "prime": "BEAVER",
+    "target": "roin",
+    "condition": "nonword"
+  },
+  {
+    "prime": "OFFICE",
+    "target": "ronket",
+    "condition": "nonword"
+  },
+  {
+    "prime": "GRIP",
+    "target": "bule",
+    "condition": "nonword"
+  },
+  {
+    "prime": "LOFT",
+    "target": "rample",
+    "condition": "nonword"
+  },
+  {
+    "prime": "STAPLER",
+    "target": "gelf",
+    "condition": "nonword"
+  },
+  {
+    "prime": "CAVERN",
+    "target": "thape",
+    "condition": "nonword"
+  },
+  {
+    "prime": "GULP",
+    "target": "shonk",
+    "condition": "nonword"
+  },
+  {
+    "prime": "TAXI",
+    "target": "troes",
+    "condition": "nonword"
+  },
+  {
+    "prime": "COUNTY",
+    "target": "thower",
+    "condition": "nonword"
+  },
+  {
+    "prime": "MOAT",
+    "target": "snave",
+    "condition": "nonword"
+  },
+  {
+    "prime": "EYEBROWS",
+    "target": "sen",
+    "condition": "nonword"
+  },
+  {
+    "prime": "FIGHTER",
+    "target": "spale",
+    "condition": "nonword"
+  },
+  {
+    "prime": "BLACKBOARD",
+    "target": "slair",
+    "condition": "nonword"
+  },
+  {
+    "prime": "SPOT",
+    "target": "stirk",
+    "condition": "nonword"
+  },
+  {
+    "prime": "HINDSIGHT",
+    "target": "shing",
+    "condition": "nonword"
+  },
+  {
+    "prime": "SUGGESTION",
+    "target": "florm",
+    "condition": "nonword"
+  },
+  {
+    "prime": "JAWS",
+    "target": "struve",
+    "condition": "nonword"
+  },
+  {
+    "prime": "WHISKEY",
+    "target": "slody",
+    "condition": "nonword"
+  },
+  {
+    "prime": "HOOP",
+    "target": "hurcess",
+    "condition": "nonword"
+  },
+  {
+    "prime": "PAT",
+    "target": "tarl",
+    "condition": "nonword"
+  },
+  {
+    "prime": "INERTIA",
+    "target": "selephore",
+    "condition": "nonword"
+  },
+  {
+    "prime": "BREEZE",
+    "target": "fent",
+    "condition": "nonword"
+  },
+  {
+    "prime": "HERO",
+    "target": "coe",
+    "condition": "nonword"
+  },
+  {
+    "prime": "LODGE",
+    "target": "gine",
+    "condition": "nonword"
+  },
+  {
+    "prime": "RAZOR",
+    "target": "woather",
+    "condition": "nonword"
+  },
+  {
+    "prime": "CHART",
+    "target": "wenner",
+    "condition": "nonword"
+  },
+  {
+    "prime": "COMMENT",
+    "target": "zofe",
+    "condition": "nonword"
+  }
+]
